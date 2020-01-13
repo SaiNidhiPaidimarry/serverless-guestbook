931e51c5ee7807885af184fcec8d7425ca50b254
SaiNidhiPaidimarry <56964938+SaiNidhiPaidimarry@users.noreply.github.com>
2020-01-13 15:34:28 +0530

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -7,7 +7,7 @@ const guestbook = {
   get() {
     return $.ajax({
       type: 'GET',
-      url: `${https://ea6c25cb.eu-gb.apigw.appdomain.cloud/guestbook}/entries`,
+      url: `${apiUrl}/entries`,
       dataType: 'json'
     });
   },
@@ -16,7 +16,7 @@ const guestbook = {
     console.log('Sending', name, email, comment)
     return $.ajax({
       type: 'PUT',
-      url: `${https://ea6c25cb.eu-gb.apigw.appdomain.cloud/guestbook}/entries`,
+      url: `${apiUrl}/entries`,
       contentType: 'application/json; charset=utf-8',
       data: JSON.stringify({
         name,
