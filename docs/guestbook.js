fb9c983bfbfb4ffc783d9ff3d41fbf992388fa85
SaiNidhiPaidimarry <56964938+SaiNidhiPaidimarry@users.noreply.github.com>
2020-01-13 15:28:24 +0530

    Update guestbook.js

--- docs/guestbook.js
+++ docs/guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://CHANGEME.us-south.apigw.appdomain.cloud/guestbook';
+const apiUrl = 'https://ea6c25cb.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
